f35964d19936e25d39ab89045fa01bb667366607
hxtchhxkxr <yoos405@naver.com>
2025-08-04 02:51:20 +0900

    튜토리얼 배경 하얗게

--- lib/main_pages/tutorial/tutorial.dart
+++ lib/main_pages/tutorial/tutorial.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 class TutorialPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('튜토리얼 페이지'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text('튜토리얼 페이지'),
+      ),
     );
   }
 }
